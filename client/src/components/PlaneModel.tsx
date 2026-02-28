6f3c0a6f773b5d4565503a2e25f6320a6a52676f
omar algamel <omeralgamel@gmail.com>
2026-02-28 21:06:26 +0200

    .

--- client/src/components/PlaneModel.tsx
+++ client/src/components/PlaneModel.tsx
@@ -1,11 +1,14 @@
 import { useRef } from "react";
 import { useFrame } from "@react-three/fiber";
-import { useScroll } from "@react-three/drei";
+import { useScroll, useGLTF } from "@react-three/drei";
 import * as THREE from "three";
 
 export default function PlaneModel({ reducedMotion }: { reducedMotion: boolean }) {
   const group = useRef<THREE.Group>(null);
   const scroll = useScroll();
+  
+  // Load the CRJ-900 model
+  const { scene } = useGLTF("/models/crj-900/scene.gltf");
 
   useFrame((state) => {
     if (!group.current) return;
@@ -73,83 +76,12 @@ export default function PlaneModel({ reducedMotion }: { reducedMotion: boolean }
     }
   });
 
-  const bodyMaterial = new THREE.MeshStandardMaterial({
-    color: 0xffffff,
-    roughness: 0.2,
-    metalness: 0.8,
-  });
-
-  const accentMaterial = new THREE.MeshStandardMaterial({
-    color: 0x00d2ff, // Cyan primary
-    roughness: 0.3,
-    metalness: 0.9,
-    emissive: 0x0055ff,
-    emissiveIntensity: 0.5,
-  });
-
-  const darkMaterial = new THREE.MeshStandardMaterial({
-    color: 0x111111,
-    roughness: 0.5,
-    metalness: 0.8,
-  });
-
   return (
-    <group ref={group} scale={[0.5, 0.5, 0.5]}>
-      {/* Fuselage */}
-      <mesh material={bodyMaterial} position={[0, 0, 0]} rotation={[Math.PI / 2, 0, 0]}>
-        <cylinderGeometry args={[0.6, 0.4, 6, 32]} />
-      </mesh>
-      
-      {/* Nose */}
-      <mesh material={bodyMaterial} position={[0, 0, 3.8]} rotation={[Math.PI / 2, 0, 0]}>
-        <coneGeometry args={[0.6, 1.6, 32]} />
-      </mesh>
-
-      {/* Cockpit / Glass */}
-      <mesh material={darkMaterial} position={[0, 0.4, 3.2]} rotation={[Math.PI / 2 - 0.2, 0, 0]}>
-        <capsuleGeometry args={[0.3, 0.3, 1, 16]} />
-      </mesh>
-
-      {/* Main Wings */}
-      <mesh material={bodyMaterial} position={[0, -0.1, 0]}>
-        <boxGeometry args={[7, 0.1, 2]} />
-      </mesh>
-      
-      {/* Wing accents */}
-      <mesh material={accentMaterial} position={[3.45, -0.1, 0.5]}>
-        <boxGeometry args={[0.1, 0.15, 2.5]} />
-      </mesh>
-      <mesh material={accentMaterial} position={[-3.45, -0.1, 0.5]}>
-        <boxGeometry args={[0.1, 0.15, 2.5]} />
-      </mesh>
-
-      {/* Engines */}
-      <mesh material={darkMaterial} position={[1.5, -0.3, -0.5]} rotation={[Math.PI / 2, 0, 0]}>
-        <cylinderGeometry args={[0.4, 0.4, 1.5, 32]} />
-      </mesh>
-      <mesh material={darkMaterial} position={[-1.5, -0.3, -0.5]} rotation={[Math.PI / 2, 0, 0]}>
-        <cylinderGeometry args={[0.4, 0.4, 1.5, 32]} />
-      </mesh>
-      
-      {/* Engine glow */}
-      <mesh position={[1.5, -0.3, -1.26]}>
-        <circleGeometry args={[0.35, 32]} />
-        <meshBasicMaterial color={0x00ffff} />
-      </mesh>
-      <mesh position={[-1.5, -0.3, -1.26]}>
-        <circleGeometry args={[0.35, 32]} />
-        <meshBasicMaterial color={0x00ffff} />
-      </mesh>
-
-      {/* Tail - Vertical */}
-      <mesh material={accentMaterial} position={[0, 1.2, -2.5]} rotation={[-0.2, 0, 0]}>
-        <boxGeometry args={[0.1, 2, 1.5]} />
-      </mesh>
-
-      {/* Tail - Horizontal */}
-      <mesh material={bodyMaterial} position={[0, 0.2, -2.7]}>
-        <boxGeometry args={[2.8, 0.1, 1]} />
-      </mesh>
+    <group ref={group} scale={[2.2, 2.2, 2.2]}>
+      <primitive object={scene} rotation={[0, 0, 0]} />
     </group>
   );
 }
+
+// Preload the model to avoid pop-in
+useGLTF.preload("/models/crj-900/scene.gltf");
